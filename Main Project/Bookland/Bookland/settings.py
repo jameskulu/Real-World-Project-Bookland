3b4d8b1a6c3cde3da3f3910e58a37b3395d358e4
James Kulu <jameskulu55@gmail.com>
2020-08-25 00:11:07 +0545

    Updated

--- Main Project/Bookland/Bookland/settings.py
+++ Main Project/Bookland/Bookland/settings.py
@@ -149,8 +149,10 @@ LOGIN_REDIRECT_URL = 'home'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = os.environ.get('email')
-EMAIL_HOST_PASSWORD = os.environ.get('password')
+# EMAIL_HOST_USER = os.environ.get('email')
+# EMAIL_HOST_PASSWORD = os.environ.get('password')
+EMAIL_HOST_USER = 'jameskulu55@gmail.com'
+EMAIL_HOST_PASSWORD = 'jookiugroftawoyr'
 EMAIL_PORT = 587
 
 # Mailchimp (Email Subscription)
